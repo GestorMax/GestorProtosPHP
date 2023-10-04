7fb5317e0da42092d1f44c8ecb99dbf026c22ad8
Hernan Castro <magehernan@gmail.com>
2023-10-04 13:07:55 -0300

    agregado de funciones para generar el metadata.

---
+++ grpc_entry_endpoints/Metadata.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace GrpcEntryEndpoints;
+
+enum AuthTypes 
+{
+	case Token;
+	case ApiKey;
+}
+
+class Metadata {
+	public static function get(AuthTypes $authType, String $auth, $entryEndpoint) 
+	{
+		switch($authType):
+			case AuthTypes::ApiKey:
+				return [
+					'X-Api-Key' => [ strtoupper(md5($auth)) ],
+					'EntryEndpoint' => $entryEndpoint, 
+				];
+				
+			case AuthTypes::Token:
+				return [
+					'authentification' => [ 'bearer ' . $auth ],
+					'EntryEndpoint' => $entryEndpoint, 
+				];
+		
+		endswitch;
+	}
+}
