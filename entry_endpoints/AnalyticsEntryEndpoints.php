14acff8173494b040c6f8c6e5fa0553033f2e3d0
Hernan Castro <magehernan@gmail.com>
2023-10-04 12:14:33 -0300

    EntryEndpoints de AnalyticsResource

---
+++ entry_endpoints/AnalyticsEntryEndpoints.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GrpcEntryEndpoints;
+
+class AnalyticsResource {
+    const ListSources = ['160,1'];
+	const ListDatas = ['160,2'];
+	const ListMetrics = ['160,3'];
+	const ListLabelsTypes = ['160,4'];
+
+	const ListLastMeasurements = ['160,11'];
+	const LastMeasurement = ['160,12'];
+	const ListMeasurements = ['160,13'];
+	const ListLastMeasurementByDate = ['160,14'];
+	const LastMultiMeasurements = ['160,15'];
+	const ListMultiMeasurements = ['160,16'];
+	const ListValues = ['160,17'];
+	const ListLastValues = ['160,18'];
+	const LastMeasurementByDate = ['160,19'];
+}
